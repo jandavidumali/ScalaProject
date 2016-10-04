ddc1ae952dbabc76bbaa983899358b79fdf4597f
jamesrombawa11 <jsrombawa@gmail.com>
2016-10-04 18:01:28 +0800

    Add files via upload
    updated code not reading both numbers and special characters!

--- src/Project.scala
+++ src/Project.scala
@@ -1,22 +1,16 @@
-
 import java.io._
 
 object Project {
   def main(args:Array[String]): Unit = {
-    val current = scala.io.Source.fromFile("file.txt")
-      .getLines
-      .flatMap(_.split("\\W+"))
-      .foldLeft(Map.empty[String, Int]) {
-        (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
-      }
+    val pw = new PrintWriter(new File("result.txt"))
+    val file = scala.io.Source.fromFile("file.txt").getLines().toList
+      .flatMap(_.split(" ")).filter(x => x.matches("[a-zA-Z]+")).sorted.groupBy(x => x)
+      .map(y => (y._1, y._2.length))
+      .toList.sorted
 
-    val pw = new PrintWriter(new File("result.txt" ))
-    current.toSeq.sortBy(_._1).foreach{
-      case (key, value) => val output = key + " = " + value
-        pw.println(output)
+    for(c <- file) {
+      pw.write(c._1 + " = " + c._2 + "\r\n")
     }
-    pw.close
-
-
+    pw.close()
   }
 }
