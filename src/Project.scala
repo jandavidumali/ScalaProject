914dbf8ef0df293b285bb24a90da83c680629e68
jamesrombawa11 <jsrombawa@gmail.com>
2016-10-04 20:11:02 +0800

    Add files via upload
    the final version

--- src/Project.scala
+++ src/Project.scala
@@ -3,14 +3,16 @@ import java.io._
 object Project {
   def main(args:Array[String]): Unit = {
     val pw = new PrintWriter(new File("result.txt"))
-    val file = scala.io.Source.fromFile("file.txt").getLines().toList
-      .flatMap(_.split(" ")).filter(x => x.matches("[a-zA-Z]+")).sorted.groupBy(x => x)
-      .map(y => (y._1, y._2.length))
-      .toList.sorted
-
-    for(c <- file) {
-      pw.write(c._1 + " = " + c._2 + "\r\n")
+    val current = scala.io.Source.fromFile("file.txt").getLines().toList
+      .flatMap(_.split(" ")).filter(x => x.matches("[a-zA-Z]+")).foldLeft(Map.empty[String, Int]) {
+      (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
     }
+        current.toSeq.sortBy(_._1).foreach{
+          case (key, value) => val output = key + " = " + value
+            pw.println(output)
+        }
     pw.close()
   }
 }
+
+
