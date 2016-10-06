20ecd165c7ac89a94ba77a7f8a56d83cec51484f
jamesrombawa11 <jsrombawa@gmail.com>
2016-10-06 20:00:51 +0800

    Add files via upload
    don't read the 1 letter words!

--- src/Project.scala
+++ src/Project.scala
@@ -4,7 +4,7 @@ object Project {
   def main(args:Array[String]): Unit = {
     val pw = new PrintWriter(new File("result.txt"))
     val current = scala.io.Source.fromFile("file.txt").getLines().toList
-      .flatMap(_.split(" ")).filter(x => x.matches("[a-zA-Z]+")).foldLeft(Map.empty[String, Int]) {
+      .flatMap(_.split(" ")).filter(x => x.matches("[a-zA-Z]+") && x.length > 1).foldLeft(Map.empty[String, Int]) {
       (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
     }
         current.toSeq.sortBy(_._1).foreach{
