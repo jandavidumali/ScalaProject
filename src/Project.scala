d56af17c8e3cc0599f6590673baf039824c8b6b3
janjankev <janumali701@gmail.com>
2016-10-07 09:32:52 +0800

    Update Project.scala

--- src/Project.scala
+++ src/Project.scala
@@ -5,7 +5,7 @@ object Project {
     val pw = new PrintWriter(new File("result.txt"))
     val current = scala.io.Source.fromFile("file.txt").getLines().toList
       .flatMap(_.split(" ")).filter(x => x.matches("[a-zA-Z]+") && x.length > 1).foldLeft(Map.empty[String, Int]) {
-      (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+      (count, word) => count + (word.toLowerCase() -> (count.getOrElse(word.toLowerCase(), 0) + 1))
     }
         current.toSeq.sortBy(_._1).foreach{
           case (key, value) => val output = key + " = " + value
